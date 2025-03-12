e7ffee9f539a689460bf75d5660e66c38912f44f
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-03-12 10:09:36 +0100

    Nueva Gama

--- JAVA/24-25/SpringBoot/jardineria/src/main/java/com/morgado/jardineria/Controllers/Admin/AdminGamasController.java
+++ JAVA/24-25/SpringBoot/jardineria/src/main/java/com/morgado/jardineria/Controllers/Admin/AdminGamasController.java
@@ -1,24 +1,47 @@
 package com.morgado.jardineria.Controllers.Admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+import com.morgado.jardineria.model.Entity.Gamas;
+import com.morgado.jardineria.model.Services.GamasService;
 
 
 @Controller
 public class AdminGamasController {
-
+    @Autowired
+    private GamasService gamasService;
+    
     @GetMapping("/admin/gamas")
-    public String gamas() {
-        System.out.println("Admin gamas");
-        return "/admin/gamas/index";
+    public String gamas(Model modelo) {
+        List<Gamas> gamas = this.gamasService.getAll(); //4
+        modelo.addAttribute("Lgamas", gamas);
+        return "/admin/gamas/listar";
     }
 
     @GetMapping("/admin/gamas/new")
-    public String newgama() {
-        System.out.println("Admin gamas New");
+    public String newgama(Model vista) {
+        Gamas gama = new Gamas();
+        vista.addAttribute("gamaForm", gama);
         return "/admin/gamas/new";
     }
     
+    @PostMapping("/admin/gamas/save")
+    public String savegama(@ModelAttribute("gamaForm") Gamas gama) {
+        System.out.println("Guardando Gams ...");
+        System.out.println(gama);
+        gama.setDescripcionTexto(gama.getDescripcionTexto().toUpperCase());
+        this.gamasService.create(gama);
+        return "redirect:/admin/gamas"; //url 
+        // return "/admin/gamas/listar"; --> plantilla
+        
+
+    }
     
 }
